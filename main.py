824f475775f5ca6be45d9965ed9f02e66068f6c4
mariofreire7708 <mariofreire7708@gmail.com>
2024-06-06 14:09:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("bonus", bonus))
     application.add_handler(CommandHandler("confirmar", confirmar))
-    application.add_handler(MessageHandler(filters.PHOTO & filters.user(user_data.keys()), receive_photo))
+    application.add_handler(MessageHandler(filters.PHOTO & filters.User(user_data.keys()), receive_photo))
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler("consultar", consultar))
     application.add_handler(CommandHandler("comment_done", comment_done))
